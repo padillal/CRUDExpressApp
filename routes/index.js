b850411918f49493d083896d1f907b2cf42c9a6d
padillal <padillal48@yahoo.com>
2020-11-14 15:36:57 -0800

    working on fixing error

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const content = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
+router.get('/', function(req, res, next) {
+  res.render('index', { messages: content , title: 'HOME PAGE', header: 'CONTENT LIST' });
+});
+
 module.exports = router;
