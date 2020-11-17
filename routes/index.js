b0d9bbe5bc1c3e0f6801f4d2c3a89b8e93a44066
padillal <padillal48@yahoo.com>
2020-11-16 22:05:59 -0800

    working on insert

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const numarray = [{
-  text: '1'
-},
-{
-  text: '2'
-},
-{
-  text: '3'
-}];
+const numarray = ['1','2','3'];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
